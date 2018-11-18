884f8a9e175956b834b465754b2b486ba277d0f5
deedeejiang <djiang@college.harvard.edu>
2018-11-18 17:10:07 -0500

    graph works !!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 
 /* main JS file */
 var dateFormatter = d3.timeFormat("%m %d %Y");
+var hourFormatter = d3.timeFormat("%H");
 var dateParser = d3.timeParse("%m/%d/%y %H:%M:%S");
 var newdateParser = d3.timeParse("%m %d %Y");
+function roundToHour(date) {
+    p = 60 * 60 * 1000; // milliseconds in an hour
+    return new Date(Math.round(date.getTime() / p ) * p);
+}
 
 // initialize data variable
 var allData = [];
 var tripsbyday = [];
+var dailydata;
 
 // Load data
 loadData();
@@ -32,7 +38,15 @@ function loadData() {
             data[i].date = dateFormatter(data[i].starttime);
             data[i].date = newdateParser(data[i].date);
 
+            // create new hour property
+            // data[i].hour = hourFormatter(data[i].starttime);
         }
+
+        // get select box value
+        var selectbox = d3.select(".selectbox").property("value");
+
+        selectbox = newdateParser(selectbox);
+        console.log(selectbox);
 
         allData = data;
         console.log(allData);
@@ -41,20 +55,49 @@ function loadData() {
             .key(function(d) {
                 return d.date;
             })
-            .rollup(function(leaves) {
-                return leaves.length;
+            .rollup(function(leaves) {return {
+                hours: d3.range(0,24).map(function() {
+                    return 0;
+                }),
+                data: leaves,
+                total: leaves.length
+            }
             })
+            // .key(function(d) {
+            //     return d.hour;
+            // })
+            // .rollup(function(leaves) {
+            //     return leaves.length;
+            // })
             .entries(allData);
 
         tripsbyday.sort(function(a, b) {
             return new Date (a.key) - new Date(b.key);
         });
 
+        // Pull daily data based on selectbox value
+        // tripsbyday.forEach(function(d) {
+        //     d.key = new Date(d.key);
+        //     // d.values.forEach(function(data) {
+        //     //     data.key = new Date(data.key);
+        //     // });
+        //
+        // });
+
         tripsbyday.forEach(function(d) {
             d.key = new Date(d.key);
+            // for (var i=0; i < d.value.data.length; i++) {
+            //     var hour = +hourFormatter(d.value.data[i].starttime);
+            //     d.value.hours[hour] ++;
+            // }
+
+            if (+selectbox == +d.key) { // can't compare objects, so convert to numbers first
+                dailydata = d;
+            }
         });
 
         console.log(tripsbyday);
+        console.log(dailydata);
 
         createVis();
     });
@@ -63,6 +106,6 @@ function loadData() {
 function createVis() {
 
     // create context bar chart with total rides per day per hour
-    var barchart = new TotalVis("totalvis", tripsbyday);
+    var barchart = new TotalVis("totalvis", dailydata);
 }
 
--- js/totalvis.js
+++ js/totalvis.js
@@ -24,8 +24,10 @@ TotalVis.prototype.initVis = function() {
         .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
     // Scales and axes
-    vis.x = d3.scaleTime()
-        .range([0, vis.width]);
+    vis.x = d3.scaleBand()
+        .rangeRound([0, vis.width])
+        .paddingInner(0.2)
+        .domain(d3.range(0,24));
 
     vis.y = d3.scaleLinear()
         .range([vis.height, 0]);
@@ -37,33 +39,33 @@ TotalVis.prototype.initVis = function() {
         .attr("class", "x-axis axis")
         .attr("transform", "translate(0," + vis.height + ")");
 
-    // Set domains
-    var minMaxY = [0, d3.max(vis.data.map(function(d) {
-        return d.value;
-    }))];
-    vis.y.domain(minMaxY);
-
-    var minMaxX = d3.extent(vis.data.map(function(d) {
-        return d.key;
-    }));
-    vis.x.domain(minMaxX);
-
-    console.log(minMaxX);
-
-    // Append path for area function
-    vis.path = vis.svg.append("path")
-        .attr("class", "area area-time");
-
-    // Define the D3 path generator
-    vis.area = d3.area()
-        .curve(d3.curveStep)
-        .x(function(d) {
-            return vis.x(d.key);
-        })
-        .y0(vis.height)
-        .y1(function(d) {
-            return vis.y(d.value);
-        });
+    // // Set domains
+    // var minMaxY = [0, d3.max(vis.data.map(function(d) {
+    //     return d.value;
+    // }))];
+    // vis.y.domain(minMaxY);
+    //
+    // var minMaxX = d3.extent(vis.data.map(function(d) {
+    //     return d.key;
+    // }));
+    // vis.x.domain(minMaxX);
+    //
+    // console.log(minMaxX);
+    //
+    // // Append path for area function
+    // vis.path = vis.svg.append("path")
+    //     .attr("class", "area area-time");
+    //
+    // // Define the D3 path generator
+    // vis.area = d3.area()
+    //     .curve(d3.curveStep)
+    //     .x(function(d) {
+    //         return vis.x(d.key);
+    //     })
+    //     .y0(vis.height)
+    //     .y1(function(d) {
+    //         return vis.y(d.value);
+    //     });
 
 
     // Filter, aggregate, modify data
@@ -73,8 +75,17 @@ TotalVis.prototype.initVis = function() {
 
 TotalVis.prototype.wrangleData = function() {
     var vis = this;
+    var hourFormatter = d3.timeFormat("%H");
 
-    vis.displayData = vis.data;
+
+    for (var i=0; i < vis.data.value.data.length; i++) {
+        var hour = +hourFormatter(vis.data.value.data[i].starttime);
+        vis.data.value.hours[hour] ++;
+    }
+
+    console.log(vis.data);
+
+    vis.displayData = vis.data.value.hours;
 
     // Update visualization
     vis.updateVis();
@@ -83,10 +94,28 @@ TotalVis.prototype.wrangleData = function() {
 TotalVis.prototype.updateVis = function() {
     var vis = this;
 
-    // Create area chart
-    vis.path
-        .datum(vis.displayData)
-        .attr("d", vis.area);
+    // Update domains
+    vis.y.domain([0, d3.max(vis.displayData)]);
+
+    var bars = vis.svg.selectAll(".bar")
+        .data(vis.displayData);
+
+    bars.enter().append("rect")
+        .attr("class", "bar")
+        .merge(bars)
+        .transition()
+        .attr("width", vis.x.bandwidth())
+        .attr("height", function(d) {
+            return vis.height - vis.y(d);
+        })
+        .attr("x", function(d, i) {
+            return vis.x(i);
+        })
+        .attr("y", function(d) {
+            return vis.y(d);
+        });
+
+    bars.exit().remove();
 
     // Append axes to chart
     vis.svg.select(".x-axis")
