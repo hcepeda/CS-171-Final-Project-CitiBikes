57c2b5913ba73ca1d216e65a35b88446fbad30f7
Sherry Gao <sherrygao@Sherrys-MacBook-Air-2.local>
2018-12-02 11:10:34 -0500

    fixed main-about js

--- js/main_about.js
+++ js/main_about.js
@@ -2,29 +2,28 @@ var dateParser = d3.timeParse("%m/%d/%y");
 var allData = [];
 
 
-  d3.queue()
+d3.queue()
     .defer(d3.csv, "data/Compiled_V2.csv")
     .defer(d3.csv, "data/sample-data.csv")
     .defer(d3.csv, "data/chord_data_3.csv")
-    .defer(d3.csv, "data/colors_yee.csv")
     .await(createVis);
 
 
-function createVis(error, aggregatedData, sampleData, chord_data, colors_df) {
+function createVis(error, aggregatedData, sampleData, chord_data) {
 
 
-  aggregatedData.forEach(function(d){
-    d.Date = dateParser(d.Date);
-    d.CumulativeMiles = +d.CumulativeMiles;
-    d.CumulativeTrips = +d.CumulativeTrips;
-    d.TripsToday = +d.TripsToday;
-    d.MilesToday = +d.MilesToday;
-    d.TotalAnnualMembers = +d.TotalAnnualMembers;
-    d.num_stations = +d.num_stations;
-    d.num_bikes = +d.num_bikes;
-  });
+    aggregatedData.forEach(function(d){
+        d.Date = dateParser(d.Date);
+        d.CumulativeMiles = +d.CumulativeMiles;
+        d.CumulativeTrips = +d.CumulativeTrips;
+        d.TripsToday = +d.TripsToday;
+        d.MilesToday = +d.MilesToday;
+        d.TotalAnnualMembers = +d.TotalAnnualMembers;
+        d.num_stations = +d.num_stations;
+        d.num_bikes = +d.num_bikes;
+    });
 
-  var LineChart = new CitiLineGraph("line_chart", aggregatedData);
-  var LineChart_new = new GeneralLine("station_bike_line", aggregatedData, "num_stations");
-  var Chord_Dia = new ChordMaker("chord_product", chord_data, colors_df);
+    var LineChart = new CitiLineGraph("line_chart", aggregatedData);
+    var LineChart_new = new GeneralLine("station_bike_line", aggregatedData, "num_stations");
+    var Chord_Dia = new ChordMaker("chord_product", chord_data);
 }
